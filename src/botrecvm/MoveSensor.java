e10ad0f4d825f2ea2f007dcb1fc5c8665fd7a53a
galaxyangelz <nguyenhoangminhduc@gmail.com>
2016-04-22 20:28:55 +0300

    Add Code

--- src/botrecvm/MoveSensor.java
+++ src/botrecvm/MoveSensor.java
@@ -13,9 +13,19 @@ public class MoveSensor {
     //TODO 1:
     //Create private members Motor and Controller objects. 
     //Create constructor and set private members as parameter values (Associations)
+    private Motor motor;
+    private Controller controller;
+    public MoveSensor (Motor motor, Controller controller) {
+        this.motor = motor;
+        this.controller = controller;
+    }
     
     //TODO 3:
     //Create method buttonPressed()
     //The method calls stop() method of Motor object and buttonPressed() method of Controller object
+    public void buttonPressed() {
+        motor.stopMotor();
+        controller.buttonPressed();
+    }
       
 }
