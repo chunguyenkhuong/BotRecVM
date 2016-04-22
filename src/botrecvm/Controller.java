2c1d669163b46d4f24dc185317643f21e83d3230
galaxyangelz <nguyenhoangminhduc@gmail.com>
2016-04-22 20:27:58 +0300

    Done TODO2

--- src/botrecvm/Controller.java
+++ src/botrecvm/Controller.java
@@ -7,7 +7,13 @@ public class Controller {
     //Create private members: sum (float), Display and Printer object members (Associations)
     //Create constructor (Observer as parameter)
     //In costructor create display and printer objects (private members above), give observer as parameter
-    
+    private float sum;
+    private Display display;
+    private Printer printer;
+    public Controller (MyObserver myobs) {
+        this.display = new Display(myobs);
+        this.printer = new Printer(myobs);
+    }    
     //TODO 3: 
     //Create buttonPressed() method: Set sum as zero and call printTicket method (printer object)
     
@@ -15,5 +21,24 @@ public class Controller {
     //Create bottleAdded() method (bottle as parameters type int)
     //Check the value of bottle parameter: 20  --> 0.2f, 15  --> 0.15f or 30  --> 0.3f (Or then INVALID)
     //Add the value to the sum and call show() method of display object (sum as parameter)
-  
+    private int bottle;
+    public void bottleAdded(int bottle) {
+        switch (bottle) {
+            case 20:
+                this.sum += 0.2f;
+                display.show(this.sum);
+                break;
+            case 15:
+                this.sum += 0.15f;
+                display.show(this.sum);
+                break;
+            case 30:
+                this.sum += 0.3f;
+                display.show(this.sum);
+                break;
+            default:
+                display.show(this.sum);
+                break;
+        };
+    }  
 }
