4593be20384e867c3311214f1d0e023bfd0467c7
galaxyangelz <nguyenhoangminhduc@gmail.com>
2016-04-22 20:27:19 +0300

    Add Code

--- src/botrecvm/Motor.java
+++ src/botrecvm/Motor.java
@@ -17,13 +17,29 @@ public class Motor extends Observable {
     //Create private member 'state' type int with initial value STOPPED (Use constants in Constants class)
     //Create constructor
     //In costructor because Motor inherits Java Observable class you can call method addObserver with parameter 'myobs' given in main
+    private int _state = 1;
+    public Motor (MyObserver myobs) {
+        this.addObserver(myobs);
+    }
     
     //TODO 2: 
     //Create start method
     //Set state as RUNNING, call setChanged(), create Event object with 0 data (Event type EVENT_MOTOR_START) and
     //finally call notifyObservers((Object)event);
+    public void start () {
+        _state = 2;
+        setChanged();
+        Event event = new Event(Constants.EVENT_MOTOR_START, 0);
+        notifyObservers((Object)event);
+    }
     
     //TODO 3: 
     //Create stopMotor method: Set state as STOPPED, then call setChanged() method, create Event object with 0 data and call notifyObservers((Object)event);
+    public void stopMotor() {
+        _state = 1;
+        setChanged();
+        Event event = new Event(Constants.EVENT_MOTOR_STOP, 0);
+        notifyObservers((Object)event);   
+    }
    
 }
